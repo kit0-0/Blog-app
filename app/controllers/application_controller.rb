03b9c811505c77d7af707d032f8e599ff432cf83
kit0-0 <kt1767@gmail.com>
2023-10-05 05:47:52 +0530

    Add current_user method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+  
+    def current_user
+      @current_user ||= User.first
+    end
+  
+    helper_method :current_user
+  end
+  
