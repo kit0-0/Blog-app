47749d0f5c6483e2dc6341822427f35780e92053
kit0-0 <kt1767@gmail.com>
2023-10-10 05:20:58 +0530

    Updated recent post method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true, allow_blank: false, uniqueness: { case_sensitive: false }
+  validates :post_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def three_most_recent_posts
+  def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
