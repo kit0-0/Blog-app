a50db69d1d32383234c3baf82294d5beb2ebe413
kit0-0 <kt1767@gmail.com>
2023-10-10 15:51:09 +0530

    Defined root_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'likes/new'
-  get 'likes/create'
-  
+  get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
+  devise_for :users
   root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new create]
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[new create]
     end
   end
-  
 end
